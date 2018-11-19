e10b52c4db3592b17c29f42d230875eac183ff2f
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2018-11-19 10:03:45 -0700

    initial commit

---
+++ src/main/java/com/galvanize/ThanksGivingProject/GameStateRepository/GameStateRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.ThanksGivingProject.GameStateRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStateRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStateRepositoryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/galvanize/ThanksGivingProject/GameStateRepository/GameStateRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.galvanize.ThanksGivingProject.GameStateRepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameStateRepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
